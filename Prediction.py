0bfa6ca68353710b59cf672fdf0aec5c5f8898e7
pagarwal884 <pagarwal1145@gmail.com>
2025-05-12 17:10:13 +0530

    Reading CSV file using pandas and also fetching statistical data from the dataset

--- Prediction.py
+++ Prediction.py
@@ -1,2 +1,16 @@
+#Importing the Dependencies
+
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+#Data Coollection and Data Processing
+
+sonar_data = pd.read_csv('Mine_Rock_Prediction//sonar data.csv' , header=None)
+
+# number of rows and columns
+
+print(sonar_data.shape)
+print(sonar_data.describe()) #describe ---> statistical measures of the data
